90a406b1601537e7eea378552fe248bac64f20bf
东南偏南 <codexu@foxmail.com>
2018-01-16 17:35:52 +0800

    init

---
+++ src/main/java/com/wkzd/springbootscaffold/SpringbootscaffoldApplication.java
@@ -0,0 +1,12 @@
+package com.wkzd.springbootscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootscaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootscaffoldApplication.class, args);
+	}
+}
