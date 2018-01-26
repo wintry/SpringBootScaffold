508a0fa6e17507ad6c06ede91c7bfd4273ada363
东南偏南 <codexu@foxmail.com>
2018-01-26 16:17:59 +0800

    update

--- src/main/java/com/wkzd/springbootscaffold/controller/HelloController.java
+++ src/main/java/com/wkzd/springbootscaffold/controller/HelloController.java
@@ -2,13 +2,20 @@ package com.wkzd.springbootscaffold.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 public class HelloController {
 
+
+    @Value("${myname}")
+    private String name;
+
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+
+        return name;
     }
 
 }
